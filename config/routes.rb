c0c3a9c27aa8638183142d05c44d9f846f9753d8
Peter Johansson <pidd69@gmail.com>
2019-06-07 17:27:46 +0200

    Fixing the delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'cocktails#index'
+  delete "destroy_cocktail/:id", to: "cocktails#destroy", as: "destroy_cocktail"
   resources :cocktails, only: [:new, :create, :show, :index, :destroy] do
     resources :doses, only: [:create, :new]
   end
